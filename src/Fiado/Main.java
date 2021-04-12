96cbc8c5af15c71372731bcb79d626ed671d65e5
Guilherme Lenon da Silva <37986306+guilhermelds@users.noreply.github.com>
2021-04-12 10:03:12 -0300

    Commit inicial

--- src/Fiado/Main.java
+++ src/Fiado/Main.java
@@ -1,16 +1,33 @@
 package Fiado;
+import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) {
 		int[] fiados = new int [10];
-		fiados[0] = 12;
-		fiados[3] = 87;
 		
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("Informe o nome do cliente: ");
+		String nome = scanner.nextLine();
+		System.out.print("Informe o valor da compra: ");
+		fiados[0] = Integer.valueOf(scanner.nextLine());
+		
+		int total = somaWhile(fiados);
+		
+		if (total > 100) {
+			System.out.println("cliente" + nome + "deve: " + total + " - ganhou brinde");
+		} else {
+			System.out.println("cliente" + nome + "deve: " + total );
+		}		
+	}
+	public static int somaWhile(int[] fiados) {
+		int i = 0;
 		int total = 0;
-		
-		System.out.println(fiados[0]);
-		
+		while (i < fiados.length) {
+			total += fiados[i];
+			i++;
+		}
+		return total;
 	}
 
-}//teste1
+}
