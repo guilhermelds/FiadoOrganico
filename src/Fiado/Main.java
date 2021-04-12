3ef96198641b8a5f5e80928752b440f5994f9c75
melhe <melhe@DESKTOP-FRMLOE6>
2021-04-12 09:42:33 -0300

    commit inicial

---
+++ src/Fiado/Main.java
@@ -0,0 +1,16 @@
+package Fiado;
+
+public class Main {
+
+	public static void main(String[] args) {
+		int[] fiados = new int [10];
+		fiados[0] = 12;
+		fiados[3] = 87;
+		
+		int total = 0;
+		
+		System.out.println(fiados[0]);
+		
+	}
+
+}
